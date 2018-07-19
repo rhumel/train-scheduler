b06dd0099a22ac5b794a6238b32bc2ac25be32c5
rhumel <rhumel@hotmail.com>
2018-07-19 18:31:24 -0400

    removed the add-child from the summit button code

--- assets/scripts/train-sched.js
+++ assets/scripts/train-sched.js
@@ -67,55 +67,57 @@
   database.ref().push(newTrain);
   
      console.log(database);
-     $("#inputTrainName").html("");
-     $("#inputDestination").html("");
-     $("#inputFrequency").html("");
-     $("#inputNextArrival").html("");
-     $("#inputTimeLeft").html("");
-   
-    
-    //firebase load
+     $("#inputTrainName").val("");
+     $("#inputDestination").val("");
+     $("#inputFrequency").val("");
+     $("#inputNextArrival").val("");
+     $("#inputTimeLeft").val("");
 
-    database.ref().on("child_added", function(snapshot) {  
-
-        console.log(snapshot.val());
-        console.log(snapshot.key);
-        console.log(snapshot.val().trainName);
-        console.log(snapshot.val().destination);
-        console.log(snapshot.val().frequency);
-        console.log(snapshot.val().nextArrival);
-        console.log(snapshot.val().timeLeft);
-
-        key=snapshot.key;
-        //Move snapshot value to newtrainSnapshot
-        newTrainSnapshot=snapshot.val();
-        
-        var newRow= $("<tr>")
-        var td0 = $("<td>").text(newTrainSnapshot.trainName);
-        var td1 =$("<td>").text(newTrainSnapshot.destination);
-        var td2 =$("<td>").text(newTrainSnapshot.frequency);
-        var td3 =$("<td>").text(newTrainSnapshot.nextArrival);
-        var td4 =$("<td>").text(newTrainSnapshot.timeLeft);
-
-        $("#tbody").append(newRow).append(td0).append(td1).append(td2).append(td3).append(td4);
-      
-        console.log($("#tbody"));        
-        console.log($("<tr>").text(newTrainSnapshot.trainName));
-        console.log($("<td>").text(newTrainSnapshot.destination));
-        console.log($("<td>").text(newTrainSnapshot.frequency));
-        console.log($("<td>").text(newTrainSnapshot.nextArrival));
-        console.log($("<td>").text(newTrainSnapshot.timeLeft));
-
-
-        
-     
-
-
-      // Handle the errors
-    }, function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-      });
-
+  
  });
+
+
+ // this is the Firebase 
+ database.ref().on("child_added", function(snapshot) {  
+
+  console.log(snapshot.val());
+  console.log(snapshot.key);
+  console.log(snapshot.val().trainName);
+  console.log(snapshot.val().destination);
+  console.log(snapshot.val().frequency);
+  console.log(snapshot.val().nextArrival);
+  console.log(snapshot.val().timeLeft);
+
+  key=snapshot.key;
+  //Move snapshot value to newtrainSnapshot
+  // var convertedDate = moment(snapshot.val().nextAririval, "MM/DD/YYYY");
+  
+ // console.log(convertedDate);
+  newTrainSnapshot=snapshot.val();
+  
+  var newRow= $("<tr>")
+  var td0 = $("<td>").text(newTrainSnapshot.trainName);
+  var td1 =$("<td>").text(newTrainSnapshot.destination);
+  var td2 =$("<td>").text(newTrainSnapshot.frequency);
+  var td3 =$("<td>").text(newTrainSnapshot.nextArrival);
+  var td4 =$("<td>").text(newTrainSnapshot.timeLeft);
+
+  $("#tbody").append(newRow).append(td0).append(td1).append(td2).append(td3).append(td4);
+
+  console.log($("#tbody"));        
+  console.log($("<tr>").text(newTrainSnapshot.trainName));
+  console.log($("<td>").text(newTrainSnapshot.destination));
+  console.log($("<td>").text(newTrainSnapshot.frequency));
+  console.log($("<td>").text(newTrainSnapshot.nextArrival));
+
+
+  
+
+
+
+// Handle the errors
+}, function(errorObject) {
+  console.log("Errors handled: " + errorObject.code);
+});
 
 

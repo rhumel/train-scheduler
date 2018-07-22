295d00bc088ba2fe54d8b9499c53bdc0bb8a2867
rhumel <rhumel@hotmail.com>
2018-07-21 22:08:00 -0400

    cleaned up code, removed unused comments and corrected formating

--- assets/scripts/train-sched.js
+++ assets/scripts/train-sched.js
@@ -1,147 +1,142 @@
 
 
 /* global moment firebase */
- 
-  //Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAiRUFveVYRzoTlqvEU2eRtujEMTCxGj5w",
-    authDomain: "train-92517.firebaseapp.com",
-    databaseURL: "https://train-92517.firebaseio.com",
-    projectId: "train-92517",
-    storageBucket: "",
-    messagingSenderId: "812298848333"
-  };
 
-  firebase.initializeApp(config);
+//Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAiRUFveVYRzoTlqvEU2eRtujEMTCxGj5w",
+  authDomain: "train-92517.firebaseapp.com",
+  databaseURL: "https://train-92517.firebaseio.com",
+  projectId: "train-92517",
+  storageBucket: "",
+  messagingSenderId: "812298848333"
+};
+
+firebase.initializeApp(config);
 
 
-  //create a variable to reference the database  
-    const database = firebase.database();
-    
-  //Initial Values
-    var trainName = "";
-    var destination = "";
-    var frequency = "";
-    var nextArrival = "";
-     var timeLeft = "";
-    var key;
+//create a variable to reference the database  
+const database = firebase.database();
+
+
+//Initial Values
+var trainName = "";
+var destination = "";
+var frequency = "";
+var nextArrival = "";
+var timeLeft = "";
+var key;
 
 
 //   -----------------------------------------------------------
- // Capture Button click
- $("#submit").on ("click", function(event) {
-     //keep the page from refreshing
-     event.preventDefault();
+// Capture  sumbit Button click 
+$("#submit").on("click", function (event) {
+  //keep the page from refreshing
+  event.preventDefault();
+
+  trainName = $("#inputTrainName").val().trim();
+  destination = $("#inputDestination").val().trim();
+  frequency = $("#inputFrequency").val().trim();
 
 
-     trainName = $("#inputTrainName").val().trim();
-     destination = $("#inputDestination").val().trim();
-     frequency = $("#inputFrequency").val().trim();
-    //  nextArrival = $("#inputNextArrival").val().trim();
-    //  timeLeft = $("#inputTimeLeft").val().trim();
-
-
-     console.log(trainName);
-     console.log(destination);
-     console.log(frequency);
-     console.log(nextArrival);
-     console.log(timeLeft);
-
-
+      console.log(trainName);
+      console.log(destination);
+      console.log(frequency);
+    
+  //New Train object for firebase.
   var newTrain = {
     trainName: trainName,
     destination: destination,
     frequency: frequency,
-    
   };
-  
+
+  // Push the initial row to firebase
   database.ref().push(newTrain);
-  
-     console.log(database);
-     $("#inputTrainName").val("");
-     $("#inputDestination").val("");
-     $("#inputFrequency").val("");
-    
-
-  
- });
+  console.log(database);
 
 
- // this adds new records to firebase 
- database.ref().on("child_added", function(snapshot) {  
-
-  console.log(snapshot.val());
-  console.log(snapshot.key);
-  console.log(snapshot.val().trainName);
-  console.log(snapshot.val().destination);
-  console.log(snapshot.val().frequency);
-
-
-  key=snapshot.key;
-// time   
-
-var currentTime = moment().format("HH:mm a");
+  //clear Input form values
+  $("#inputTrainName").val("");
+  $("#inputDestination").val("");
+  $("#inputFrequency").val("");
 
 
 
-
-$("#local-time").text("Current Local Time: "+currentTime);
-//setInterval(timeCalculation,60000);
-  
-console.log(currentTime);
+});
 
 
-  newTrainSnapshot=snapshot.val();
+// Adds new records to firebase 
+database.ref().on("child_added", function (snapshot) {
 
-  nextArrival= moment(currentTime, "HH:mm").add(newTrainSnapshot.frequency,"minutes").format("HH:mm");
+      console.log(snapshot.val());
+      console.log(snapshot.key);
+      console.log(snapshot.val().trainName);
+      console.log(snapshot.val().destination);
+      console.log(snapshot.val().frequency);
 
+  //Grab Key so that it can be used to delete record
+  key = snapshot.key;
 
-var convertedDate = moment(nextArrival, "HH:mm a").format("LT");
+  // grab current time to use to calulate diff in time arrival
+  var currentTime = moment().format("HH:mm a");
 
-var cTime = moment(nextArrival, "hh:mm a");
-timeLeft = -1* (moment(moment()).diff(cTime, "minutes"));
+//place time on table
+  $("#local-time").text("Current Local Time: " + currentTime);
+      console.log(currentTime);
 
-console.log(convertedDate);
-console.log(cTime);
-console.log(timeLeft);
+//add frequency time
+  newTrainSnapshot = snapshot.val();
+  nextArrival = moment(currentTime, "HH:mm").add(newTrainSnapshot.frequency, "minutes").format("HH:mm");
 
+//Calulate mins left
+  var convertedDate = moment(nextArrival, "HH:mm a").format("LT");
+  var cTime = moment(nextArrival, "hh:mm a");
+  timeLeft = -1 * (moment(moment()).diff(cTime, "minutes"));
 
-  var newRow= $("<tr>")
+      console.log(convertedDate);
+      console.log(cTime);
+      console.log(timeLeft);
+
+  //create data row for the table
+  var newRow = $("<tr>")
   var td0 = $("<td>").text(newTrainSnapshot.trainName);
-  var td1 =$("<td>").text(newTrainSnapshot.destination);
-  var td2 =$("<td>").text(newTrainSnapshot.frequency);
-  var td3 =$("<td>").text(nextArrival);
-  var td4 =$("<td>").text(timeLeft);
+  var td1 = $("<td>").text(newTrainSnapshot.destination);
+  var td2 = $("<td>").text(newTrainSnapshot.frequency);
+  var td3 = $("<td>").text(nextArrival);
+  var td4 = $("<td>").text(timeLeft);
 
   var button = $("<button>");
-          button.addClass("del-btn");
-          button.attr("data-key", key);
-          button.text("Delete");
-          
+  button.addClass("del-btn");
+  button.attr("data-key", key);
+  button.text("Delete");
+
   newRow.append(td0).append(td1).append(td2).append(td3).append(td4).append(button);
   $("#tbody").append(newRow)
 
-  console.log($("#tbody"));        
-  console.log($("<tr>").text(newTrainSnapshot.trainName));
-  console.log($("<td>").text(newTrainSnapshot.destination));
-  console.log($("<td>").text(newTrainSnapshot.frequency));
-  console.log($("<td>").text(newTrainSnapshot.nextArrival));
+      console.log($("#tbody"));
+      console.log($("<tr>").text(newTrainSnapshot.trainName));
+      console.log($("<td>").text(newTrainSnapshot.destination));
+      console.log($("<td>").text(newTrainSnapshot.frequency));
+      console.log($("<td>").text(newTrainSnapshot.nextArrival));
 
-// Handle the errors
-}, function(errorObject) {
+  // Handle the errors
+}, function (errorObject) {
   console.log("Errors handled: " + errorObject.code);
 });
-//on click for delete button
 
-$(document).on("click", ".del-btn", function() {
+//on click for delete button
+$(document).on("click", ".del-btn", function () {
   //keep the page from refreshing
   event.preventDefault();
 
-    var deleteKey = $(this).attr("data-key");
+  //get the Key from the butoom
+  var deleteKey = $(this).attr("data-key");
+  console.log(deleteKey);
 
-    console.log(deleteKey);
-    database.ref().child(deleteKey).remove();
+  //delete data row from parent
+  database.ref().child(deleteKey).remove();
 
-    $(this).parent().remove();
+  //delete the row from the table
+  $(this).parent().remove();
 
 });
